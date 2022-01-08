15f9f86e8bb0e769bf03a1d96aafb13507dae673
Alex <2382854p@student.gla.ac.uk>
2022-01-08 23:58:43 +0000

    [WT] got to point where datagram reordering works for 100x100 feed

--- webtransport/public/script.js
+++ webtransport/public/script.js
@@ -1,5 +1,5 @@
-const WIDTH = 300;
-const HEIGHT = 300;
+const WIDTH = 100;
+const HEIGHT = 100;
 let sentCount = 0;
 let recvCount = 0;
 let sentFramesCount = 0;
@@ -7,11 +7,14 @@ let recvFramesCount = 0;
 const FRAMESIZE = WIDTH * HEIGHT * 4;
 const HEADERSIZE = 16;
 const connections = {};
-let prevSequenceNumber = -1;
 var queue = [];
 var frameNumber = 0;
-let frameCount = 0;
+var prevFrameNumber = 0;
 let dict = {};
+var prevSequenceNumber = -1;
+let counter = 0;
+let keys = null;
+let prevValue = null;
 
 const videoGrid = document.getElementById('video-grid');
 
@@ -29,7 +32,7 @@ let currentTransport, streamNumber, currentTransportDatagramWriter;
 // "Connect" button handler.
 //   const url = document.getElementById('url').value;
 try {
-    var transport = new WebTransport('https://web-platform.test:64224');
+    var transport = new WebTransport('https://web-platform.test:59040');
     addToEventLog('Initiating connection...');
 } catch (e) {
     addToEventLog('Failed to create connection object. ' + e, 'error');
@@ -65,6 +68,14 @@ transport.closed
 // document.forms.sending.elements.send.disabled = false;
 // document.getElementById('connect').disabled = true;
 
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 // "Send data" button handler.
 async function sendData(canvasContext, video, streamId, sequenceNumber, ts) {
     let encoder = new TextEncoder('utf-8');
@@ -79,29 +90,29 @@ async function sendData(canvasContext, video, streamId, sequenceNumber, ts) {
             const imgData = canvasContext.getImageData(0, 0, WIDTH, HEIGHT);
 
             ts = Date.now();
-            frameNumber = sentFramesCount;
             sentFramesCount++;
+            const frameNumber = sentFramesCount;
+            // prevSequenceNumber = -1;
             showStats();
             let offset = 0;
 
-            let size = 1008;
-
-            let chunk = imgData.data.slice(offset, offset + size);
-    
-            let writeBuffer = new Uint8ClampedArray(HEADERSIZE + chunk.length);
-            const dv = new DataView(writeBuffer.buffer, 0); // is dataview too slow?
-
             // sends datagrams
             while (true) {
+                let size = 1008;
                 if (offset + size > imgData.data.length) {
                     size = imgData.data.length - offset;
                 }
                 //console.log("SENDING", size);   // make sure this matches below
                 await currentTransportDatagramWriter.ready;
-                chunk = imgData.data.slice(offset, offset + size);
+                const chunk = imgData.data.slice(offset, offset + size);
+
+                let writeBuffer = new Uint8ClampedArray(HEADERSIZE + chunk.length);
+                const dv = new DataView(writeBuffer.buffer, 0); // is dataview too slow?
 
                 // writeBuffer.length = (HEADERSIZE + chunk.length);
                 // console.log(writeBuffer.length);
+
+                sequenceNumber++;
 
                 writeBuffer.set(chunk, HEADERSIZE);
                 
@@ -113,7 +124,6 @@ async function sendData(canvasContext, video, streamId, sequenceNumber, ts) {
                 // var data = encoder.encode(dv);
 
                 await currentTransportDatagramWriter.write(dv); // find out if all data is being written
-                sequenceNumber++;
                 sentCount++;
                 showStats();
                 offset += size;
@@ -162,126 +172,115 @@ async function readDatagrams(transport) {
     }
     let decoder = new TextDecoder('utf-8');
     try {
-
+      
       while (true) {
+
+        //console.log(dict);
+        //console.log(keys);
         var { value, done } = await reader.read();
         if (done) {
           addToEventLog('Done reading datagrams!');
           return;
         }
 
-        const dataSize = value.length - HEADERSIZE;
-        let dv = new DataView(value.buffer);  
+        const dv = new DataView(value.buffer);  
         const streamId = dv.getUint32(0);
         const sequenceNumber = dv.getUint32(4);
         const eof = dv.getUint32(8);
         const frameNumber = dv.getUint32(12);
 
-        if (!connections[streamId]) {
-          document.querySelector('#logs').innerHTML += `User ${streamId} connected\n`;    // if user's ID is not already present, show as connected
-          connections[streamId] = createRemoteView(streamId);                                   // create video feed       
+        if (!dict[frameNumber]) {
+          dict[frameNumber] = []
+        }
+
+        // if (frameNumber >= prevFrameNumber) { // problem line? yes, it just stops the previous frame when the first one comes in
+        prevFrameNumber = frameNumber;
+        if (!(value in dict)) {
+          dict[frameNumber].push(value);
+        }
+
+        //console.log(frameNumber);
+        //console.log(dict);
+
+        //console.log(dict);
+
+        keys = Object.keys(dict);
+        keys.sort(function(a, b) {
+            return a - b;
+        });
+
+        //console.log(keys);
+
+        let idx = 0;
+        value = dict[keys[idx]].shift();
+        if (!value) {
+          counter++;
+          if (counter == 100) {
+            console.log("backup render");
+            render(connections[streamId]);
+            delete dict[keys[idx]];
+            //render(connections[streamId]);
+            counter = 0;
+            // if (!value) {
+            //   continue;
+            // }
+          }
+          continue;
+        }
+
+        const dv2 = new DataView(value.buffer);  
+        const streamId2 = dv2.getUint32(0);
+        const sequenceNumber2 = dv2.getUint32(4);
+        const eof2 = dv2.getUint32(8);
+        const frameNumber2 = dv2.getUint32(12);
+
+        //console.log(value);
+
+        const dataSize = value.length - HEADERSIZE;
+
+        if (!connections[streamId2]) {
+          document.querySelector('#logs').innerHTML += `User ${streamId2} connected\n`;    // if user's ID is not already present, show as connected
+          connections[streamId2] = createRemoteView(streamId2);                                   // create video feed       
         }
         //const { context, buffer, bufferOffset } = connections[streamId];
 
-        if (prevSequenceNumber != (sequenceNumber - 1) && sequenceNumber != 0) {
-          console.log(prevSequenceNumber, sequenceNumber, dataSize)
-          connections[streamId].bufferOffset = (connections[streamId].bufferOffset + dataSize*(sequenceNumber-prevSequenceNumber)-1008);
+        //console.log(frameNumber, prevSequenceNumber, sequenceNumber);
+
+        // if (prevSequenceNumber != (sequenceNumber - 1) && sequenceNumber != 0 && prevSequenceNumber <= sequenceNumber) {
+        //   //console.log(prevSequenceNumber, sequenceNumber, dataSize)
+        //   connections[streamId].bufferOffset = (connections[streamId].bufferOffset + dataSize*(sequenceNumber-prevSequenceNumber));
+        // }
+
+        //console.log(frameNumber, prevSequenceNumber, sequenceNumber, eof);
+
+        prevSequenceNumber = sequenceNumber2;
+
+        if (connections[streamId2].bufferOffset + dataSize <= FRAMESIZE) {          // accounts for when data size is too big
+          connections[streamId2].buffer.set(value.slice(HEADERSIZE), connections[streamId2].bufferOffset);
+          connections[streamId2].bufferOffset += dataSize;
         }
 
-        prevSequenceNumber = sequenceNumber;
+        console.log(frameNumber2, sequenceNumber2, connections[streamId2].bufferOffset, dataSize, eof2);     // eof and bufferOffset do not line up?
 
-        if (connections[streamId].bufferOffset + dataSize <= FRAMESIZE) {          // accounts for when data size is too big
-          connections[streamId].buffer.set(value.slice(HEADERSIZE), connections[streamId].bufferOffset);
-          connections[streamId].bufferOffset += dataSize;
+        //console.log(dict);
+
+        //if (connections[streamId].bufferOffset + dataSize >= FRAMESIZE) {  // if end of video?
+        //if ((eof2 == 1 && connections[streamId2].bufferOffset < FRAMESIZE) || connections[streamId2].bufferOffset >= FRAMESIZE) {
+        if (eof2 == 1) {
+          (console.log("main render"))
+          render(connections[streamId2]);
+          delete dict[keys[idx]];
         }
 
-        if (eof == 1) {  // if end of video?
-          render(connections[streamId]);
-          connections[streamId].bufferOffset = 0;
-        }
+        
 
-        console.log(sequenceNumber, connections[streamId].bufferOffset, dataSize, eof);     // eof and bufferOffset do not line up?
 
         recvCount ++;
         showStats();
-
-        // let dv = new DataView(value.buffer);  
-        // let frameNumber = dv.getUint32(12);
-
-        // if (!dict[frameNumber]) {
-        //   dict[frameNumber] = new Array()
-        // }
-
-        // console.log(dict);
-
-        // dict[frameNumber].push(value);
-
-        // var keys = Object.keys(dict)
-
-        // for (var i = 0; i < keys.length; i++) {
-        //   if (dict[keys[i]].length == 0) {
-        //       delete dict[keys[i]];
-        //   }
-        // }
-
-        // if (dict.length != 0) {
-        //   keys = Object.keys(dict);
-        //   keys.sort(function(a, b) {
-        //       return a - b;
-        //   });
-
-        //   // var values = Object.keys(dict).map(function(key){
-        //   //   return dict[key];
-        //   // });
-
-        //   var loops = dict[keys[0]].length
-
-        //   for (var i = 0; i < loops; i++) {
-
-        //     value = dict[keys[0]][i];
-        //     dict[keys[0]].pop(value);
-
-        //     const dataSize = value.length - HEADERSIZE;
-        //     let dv = new DataView(value.buffer);  
-        //     const streamId = dv.getUint32(0);
-        //     const sequenceNumber = dv.getUint32(4);
-        //     const eof = dv.getUint32(8);
-        //     const frameNumber = dv.getUint32(12);
-
-        //     if (!connections[streamId]) {
-        //       document.querySelector('#logs').innerHTML += `User ${streamId} connected\n`;    // if user's ID is not already present, show as connected
-        //       connections[streamId] = createRemoteView(streamId);                                   // create video feed       
-        //     }
-        //     //const { context, buffer, bufferOffset } = connections[streamId];
-
-        //     if (prevSequenceNumber != (sequenceNumber - 1) && sequenceNumber != 0) {
-        //       //console.log(prevSequenceNumber, sequenceNumber, dataSize)
-        //       connections[streamId].bufferOffset = (connections[streamId].bufferOffset + dataSize*(sequenceNumber-prevSequenceNumber)-1008);
-        //     }
-
-        //     //console.log(connections[streamId].bufferOffset);
-
-        //     prevSequenceNumber = sequenceNumber;
-
-        //     if (connections[streamId].bufferOffset + dataSize <= FRAMESIZE) {          // accounts for when data size is too big
-        //       connections[streamId].buffer.set(value.slice(HEADERSIZE), connections[streamId].bufferOffset);
-        //       connections[streamId].bufferOffset += dataSize;
-        //     }
-
-        //     else if (connections[streamId].bufferOffset + dataSize >= FRAMESIZE || eof == 1) {  // if end of video?
-        //       render(connections[streamId]);
-        //       connections[streamId].bufferOffset = 0;
-        //     }
-
-        //     //console.log(sequenceNumber, connections[streamId].bufferOffset, dataSize, eof);     // eof and bufferOffset do not line up?
-
-        //     recvCount ++;
-        //     showStats();
-
-            //addToEventLog('Datagram received');
-          //}
-        //}
+        //console.log("test3");
+        //addToEventLog('Datagram received');
       }
+      //}
     } catch (e) {
       addToEventLog('Error while reading datagrams: ' + e, 'error');
     }
@@ -327,7 +326,7 @@ async function readFromIncomingStream(stream, number) {
 }
 
 const createRemoteView = (id) => {
-    console.log("creating remote view");
+    //console.log("creating remote view");
     const canvas = document.createElement('canvas');
     canvas.setAttribute("id", `remote_${id}`);
     canvas.width = WIDTH;
@@ -342,9 +341,11 @@ const createRemoteView = (id) => {
 {/* renders received media data */}
 async function render(view){
     recvFramesCount++;
-    // console.log("-----------------");
+    console.log("----------------- RENDER FRAME -----------------");
     // console.log("RENDER FRAME");
     view.context.putImageData(new ImageData(view.buffer, WIDTH, HEIGHT), 0, 0);
+    view.bufferOffset = 0;
+    //view.buffer = new Uint8ClampedArray(FRAMESIZE);;
 };
   
 function addToEventLog(text, severity = 'info') {
@@ -372,11 +373,11 @@ async function startMedia(datagramWriter) {
     video.play();   // unsure what this does
 
     let streamId = Math.floor(Math.random() * 4294967295);  // should use uuid instead
-    let sequenceNumber = 0;
+    var sequenceNumber = 0;
     let ts = Date.now();
 
     const canvasContext = document.createElement("canvas").getContext("2d");
     setInterval(async () => {
         sendData(canvasContext, video, streamId, sequenceNumber, ts);
-    }, 5000);
+    }, 100);
 };
